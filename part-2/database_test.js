97418d6a54d9696a5ed0d93da13f828e659b28cb
HJBowers <HJBowers@gmail.com>
2017-08-08 15:45:50 -0700

    Reformatted the database queries after realizing they weren't easily testable

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -1,36 +1,46 @@
 const mocha = require('mocha')
 const chai = require('chai')
 const expect = chai.expect
-const database = require("./database")
+const database = require('./database')
 
 describe('Command: product-list\n', () => {
-  it("1. List all products from a section", function() {
-    const section = 'dairy'
+  it('1. List all products from a section as array', function(done) {
+    const section = 'bulk'
     database.listProductsBySection(section)
-      .then((fullListBySection) => {
-        expect(fullListBySection).to.be.a('Array')
+      .then(function(fullListBySection) {
+        expect(fullListBySection.rows).to.be.a('Array')
         done()
       })
       .catch(console.log)
   })
 
-  describe("Command: shopper-orders", function() {
-    it("1. List all orders for a given shopper id, returns the order id, and the total cost of the order", function() {
+  it('2. List the number of products from a section', function(done) {
+    const section = 'bulk'
+    database.listProductsBySection(section)
+      .then(function(fullListBySection) {
+        expect(fullListBySection.rows.length).to.equal(3)
+        done()
+      })
+      .catch(console.log)
+  })
+
+  describe('Command: shopper-orders\n', function() {
+    it('1. List all orders for a given shopper id, returns the order id, and the total cost of the order', function(done) {
       const shopperId = '1'
       database.ordersByShopper(shopperId)
         .then((ordersAndTotals) => {
-          expect(ordersAndTotals).to.be.a('Array')
+          expect(ordersAndTotals.rows).to.be.a('Array')
           done()
         })
         .catch(console.log)
     })
   })
 
-  describe("Command: real-shoppers", function() {
-    it("1. List all the shoppers that have at least 1 order, and also display the number of orders for them", function() {
+  describe('Command: real-shoppers\n', function() {
+    it('1. List all the shoppers that have at least 1 order, and also display the number of orders for them', function(done) {
       database.shoppersWhoHaveOrdered()
         .then((shoppersAndOrders) => {
-          expect(shoppersAndOrders).to.be.a('Array')
+          expect(shoppersAndOrders.rows).to.be.a('Array')
           done()
         })
         .catch(console.log)
--- part-2/store.js
+++ part-2/store.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 const { listProductsBySection, ordersByShopper, shoppersWhoHaveOrdered } = require('./database.js')
+const {productList, shopperOrders, realShoppers} = require('./tableFormatting')
 
 const run = (command, argInput) => {
   if (command === 'product-list') {
     listProductsBySection(argInput)
+    .then(res => {
+      productList(res.rows)
+    })
+    .catch( e => console.error(e.stack))
+    .then(() => client.end())
   } else if (command === 'shopper-orders') {
     ordersByShopper(argInput)
+    .then(res => {
+      shopperOrders(res.rows)
+    })
+    .catch( e => console.error(e.stack))
+    .then(() => client.end())
   } else if (command === 'real-shoppers') {
     shoppersWhoHaveOrdered()
+    .then(res => {
+      realShoppers(res.rows)
+    })
+    .catch( e => console.error(e.stack))
+    .then(() => client.end())
   } else if (command) {
     console.log("Invalid Command")
   }
